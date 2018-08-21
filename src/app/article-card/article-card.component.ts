ec4c7b932b78d6a49e74b9674c5d16f4b09c5373
CT-Kyle <ckyle@smu.edu>
2018-08-21 14:22:17 -0600

    added mobile styles

--- src/app/article-card/article-card.component.ts
+++ src/app/article-card/article-card.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit, NgModule } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
 
-
-@NgModule({
-  imports: [MatCardModule]
-})
 @Component({
   selector: 'app-article-card',
   templateUrl: './article-card.component.html',
